5c8363774ed61c23e989a9438f01e473180a2cef
g21turbo <dg21stew@gmail.com>
2024-01-01 22:37:25 -0800

    formatting practice

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String bulletIt = "Print a bullet List:" +
-                "\u2022 First Point" +
-                "\u2022 Second Point";
+        String bulletIt = "Print a Bulleted List: \n" +
+                "\t\u2022 First Point \n" +
+                "\t\t\u2022 Sub Point";
 
         System.out.println(bulletIt);
 
+        String textBlock = """
+                Print a Bulleted List:
+                            \u2022 First Point
+                                \u2022 Sub Point""";
+
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d\n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d", age, yearOfBirth);
+
+        System.out.printf("Your age is %.4f%n", (float)age);
+
+        for (int i = 1; i <= 10000; i *= 10){
+            System.out.printf("printing %6d %n", i);
+        }
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
 
     }
 }
